1a5e53ea8e6bb331faa95baaa7ee944e8466a099
legend80s <liuchzong@qq.com>
2025-06-09 17:24:05 +0800

    feat: add validator to env variables

--- utils/env_settings.py
+++ utils/env_settings.py
@@ -1,9 +1,28 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import field_validator
 
 
 class Settings(BaseSettings):
     chat_id: str
     authorization: str
+
+    @field_validator("chat_id")
+    @classmethod
+    def validate_str(cls, value: str) -> str:
+        count = len(value)
+        if count != 20:
+            raise ValueError("str must be 20 characters long")
+        if not value.isalnum():
+            raise ValueError("str must be alphanumeric")
+        return value
+
+    @field_validator("authorization")
+    @classmethod
+    def validate_authorization(cls, value: str) -> str:
+        # print(f"{value=}")
+        if not value.startswith("Bearer "):
+            raise ValueError("Authorization must start with 'Bearer '")
+        return value
 
     model_config = SettingsConfigDict(env_file=".env")
 
@@ -14,6 +33,8 @@ def get_settings() -> Settings:
 
 
 if __name__ == "__main__":
-    settings = get_settings()
-    print(f"{settings.authorization=}")
-    print(f"{settings.chat_id=}")
+    try:
+        settings = get_settings()
+        print(settings.model_dump())
+    except ValueError as e:
+        print(e)
